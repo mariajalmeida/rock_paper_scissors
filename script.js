e81d1dd0ba09613cbf20f6202c5bff8c96fff70b
mariajalmeida <mari9d91@stud.kea.dk>
2021-02-04 19:33:32 +0100

    hands changing accordingly

--- script.js
+++ script.js
@@ -44,20 +44,54 @@ function computerDecision() {
 }
 
 function compareResults(r) {
+  const loser = document.querySelector("#lose");
+  const winner = document.querySelector("#win");
+  const tie = document.querySelector("#draw");
+
   if (computer == player) {
     console.log("it's a draw");
+    tie.classList.remove("hidden");
   } else if (computer == "rock" & player == "paper") {
     console.log("player wins");
+    winner.classList.remove("hidden");
   } else if (computer == "paper" & player == "rock") {
     console.log("player loses");
+    loser.classList.remove("hidden");
   } else if (computer == "scissors" & player == "paper") {
     console.log("player loses");
+    loser.classList.remove("hidden");
   } else if (computer == "scissors" & player == "rock") {
     console.log("player wins");
+    winner.classList.remove("hidden");
    } else if (player == "scissors" & computer == "paper") {
     console.log("player wins");
+    winner.classList.remove("hidden");
    } else if (player == "scissors" & computer == "rock") {
     console.log("player loses");
+    loser.classList.remove("hidden");
    } 
+
+   const user = document.querySelector("#player1.player");
+   const handPaper = "url('hand_paper.png')";
+   const handRock = "url('hand_rock.png')";
+   const handScissors = "url('hand_scissors.png')";
+   if (player == "paper") {
+      user.style.backgroundImage = handPaper;
+      console.log("help");
+   } else if (player == "scissors") {
+      user.style.backgroundImage = handScissors;
+   } else if (player == "rock") {
+      user.style.backgroundImage = handRock;
+ }
+
+const machine = document.querySelector("#player2.player");
+ if (computer == "paper") {
+  machine.style.backgroundImage = handPaper;
+  console.log("help");
+} else if (computer == "scissors") {
+  machine.style.backgroundImage = handScissors;
+} else if (computer == "rock") {
+  machine.style.backgroundImage = handRock;
+}
   
 }
